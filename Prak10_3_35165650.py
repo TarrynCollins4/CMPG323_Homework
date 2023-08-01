8836a040f824f424836ad87c08565560cd8fd347
Tarryn <collinstarryn4@gmail.com>
2023-08-01 08:37:16 +0200

    Create Prak10_3_35165650.py

---
+++ Prak10_3_35165650.py
@@ -0,0 +1,14 @@
+# Tarryn Collins
+print("For the calculation of prime numbers;")
+start_number = int(input("   supply a positive starting number: "))
+end_value = int(input("   and a positive ending value: "))
+
+print("Prime numbers between" , start_number, "and", end_value, "are:")
+for num in range (start_number,end_value +1):
+    if num>1:
+        for i in range (2,num):
+            if(num%i)==0:
+               break
+        else:
+            print(num)
+     
